09ed8926417c9e491daa6bfcf1c6d4006f2e21d6
RianOntu <rianontu13@gmail.com>
2023-04-14 21:59:05 +0600

    created applied jobs filter option

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SingleAppliedJob from '../SingleAppliedJob/SingleAppliedJob';
 import './AppliedJobs.css'
 
 const AppliedJobs = () => {
     const appliedJobs=JSON.parse(localStorage.getItem('appliedJobs'));
+    const [jobs,setJobs]=useState(appliedJobs)
+    const displayOnsite=()=>{
+        const onsite=appliedJobs.filter(job=>job.location==="onsite")
+        console.log(onsite);
+        setJobs(onsite);
+    }
+    const displayRemote=()=>{
+        const remote=appliedJobs.filter(job=>job.location==="remote")
+        console.log(remote);
+        setJobs(remote)
+    }
+    const displayAll=()=>{
+        setJobs(appliedJobs)
+    }
 
     return (
         <div>
@@ -11,10 +25,18 @@ const AppliedJobs = () => {
                 <h3 style={{textAlign:"center"}}>Applied Jobs</h3>
             </div>
            <div className="container">
+            <div className="end mb-5">
+                <h5><b>Filter By:</b></h5>
+                <div className="d-flex">
+                    <button onClick={()=>displayOnsite()} className='btn btn-success'>Onsite</button>
+                    <button onClick={()=>displayRemote()} className='btn btn-primary margin '>Remote</button>
+                    <button onClick={()=>displayAll()} className='btn btn-warning margin '>All</button>
+                </div>
+            </div>
             <div className="row">
             <div className="col-12">
                 {
-                    appliedJobs.map(appliedJob=><SingleAppliedJob appliedJob={appliedJob}></SingleAppliedJob>)
+                    jobs.map(appliedJob=><SingleAppliedJob appliedJob={appliedJob}></SingleAppliedJob>)
                 }
             </div>
             </div>
